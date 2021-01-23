1142353350143275b9dd10421b4184fb6e864ad5
adamkahn33 <70861676+adamkahn33@users.noreply.github.com>
2021-01-23 13:15:25 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'codingbootcamp.cze9dquabwlh.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'root'
+remote_db_pwd = 'Dartlings!'
+remote_db_name = 'gwsis'
